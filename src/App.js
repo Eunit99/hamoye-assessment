96bd6aed47d191af5490651115b076fb2f97a8e4
Emmanuel Uchenna <eunithelp@gmail.com>
2022-04-09 11:46:55 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from '../src/components/card/Card';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<div className="container mt-5">
+				<div className="row mt-5">
+					<Card />
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/card/Card.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import { allAirportData } from '../../data/data';
+
+
+
+function Card(props) {
+
+	const [currentTime, setCurrentTime] = useState(new Date());
+	const [fetchData, setFetchData] = useState("");
+	const timeStamp = currentTime.getTime() / 1000;
+
+	useEffect(() => {
+		// const fetchURL = " http://localhost:8888/flight";
+		const fetchURL = `https://opensky-network.org/api/flights/all?begin=${timeStamp}&end=${timeStamp}`;
+
+		console.log(fetchURL);
+
+		
+		fetch(fetchURL).then((res) => {
+			if (!res.ok) {
+				console.error("Error while trying to fetch");
+				throw "Error occurred while trying to fetch result";
+			}
+			try {
+				res.json()
+					.then((data) => {
+						console.log(data);
+						setFetchData(data);
+					});
+			}
+			catch (err) {
+				console.error(err);
+			}
+		});
+	}, []);
+
+	useEffect(() => {
+		setCurrentTime(currentTime);
+	}, [currentTime]);
+
+
+
+	return (
+		<>
+			{
+				allAirportData.map((card, index) => (
+					<div className="col col-12 col-md-3" key={index}>
+						<div className="card">
+							<div className="card-body">
+								<h5 className="card-title title p-3">Airport: <b>{card.estDepartureAirport} </b> </h5>
+								<p className="card-text">Time <b>{currentTime.toUTCString()} </b></p>
+								<p className="center">Arriving <b>{card.arrivalAirportCandidatesCount} </b></p>
+								<p className="center">Departing <b>{card.departureAirportCandidatesCount} </b></p>
+							</div>
+						</div>
+					</div>
+				))
+			}
+		</>
+	);
+}
+
+export default Card;
